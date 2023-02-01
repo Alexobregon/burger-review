b8200e3a4ea9425249ae5c8981484f0d5f5c2076
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-01 14:32:49 -0800

    seting up react routes

--- Burger-review/src/App.js
+++ Burger-review/src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {  Routes, Route } from "react-router-dom";
+import React from "react";
+import NavBar from "./NavBar";
+import About from "./About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={"https://cdn.pixabay.com/photo/2020/03/21/11/17/burger-4953465__340.jpg"} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div>
+    <NavBar />
+    <Routes>
+      <Route exact path='/About'>
+        <About />
+      </Route>
+    </Routes>
+  </div>
+
+
+
+
   );
 }
 
