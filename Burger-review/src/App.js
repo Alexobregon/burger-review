a8a693d8f491b3a018e1b09a8bc6de8b26683319
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-01 16:41:10 -0800

    Redoing setup

--- Burger-review/src/App.js
+++ Burger-review/src/App.js
@@ -1,41 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import {  Routes, Route } from "react-router-dom";
-import React from "react";
-import NavBar from "./NavBar";
-import About from "./About";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={"https://cdn.pixabay.com/photo/2020/03/21/11/17/burger-4953465__340.jpg"} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-    <div>
-    <NavBar />
-    <Routes>
-      <Route exact path='/About'>
-        <About />
-      </Route>
-    </Routes>
-  </div>
-
-
-
-
+    <div className="App">
+      <header className="App-header">
+        <img src={"https://t3.ftcdn.net/jpg/00/69/85/64/360_F_69856461_O8p56mlDwWo0mXFswcYbGbP7Ihlbimiw.jpg"} className="App-logo" alt="logo" />
+        <p>
+         Burger Review!
+        </p>
+       
+      </header>
+    </div>
   );
 }
 
