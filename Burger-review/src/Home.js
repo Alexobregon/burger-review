b47f38a48bce87f5a790f36cab41de5657ddb5ba
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-02 16:50:20 -0800

    main burger image on screen

--- Burger-review/src/Home.js
+++ Burger-review/src/Home.js
@@ -2,9 +2,16 @@ import React from "react";
 
 function Home() {
   return (
+    <>
   <div>
     <h1>Home!</h1>;
     </div> 
+    <div>
+    <img src={'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80'}
+     id="main-burger" alt="Buger"></img>
+
+    </div>
+    </>
     );
 }
 
