339e713ff2992423e9b3c5a4bfd5456f009d6a41
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-03 17:02:03 -0800

    added css for navbar when on About page

--- Burger-review/src/About.js
+++ Burger-review/src/About.js
@@ -1,10 +1,51 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "20px",
+  margin: "0 6px 6px",
+  //background: "blue",
+  textDecoration: "none",
+  color: "black",
+};
 
 function About() {
   return (
+    <>
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        // activeStyle={{
+        //   background: "darkblue",
+        // }}
+        activeStyle={{
+          textDecoration: "underline"
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          textDecoration: "underline",
+        }}
+      >
+        About
+      </NavLink>
+      
+    </div>
+
+    
   <div>
   <h2>This is my about component!!</h2>
   </div>
+  </>
   );
 }
 
