e2d85d68cf80c3f001bba0e8e0af6f1f2a4572b3
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-07 14:26:55 -0800

    added rating page basics

---
+++ Burger-review/src/Rating.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "20px",
+  margin: "0 6px 6px",
+  //background: "blue",
+  textDecoration: "none",
+  color: "black",
+};
+
+function Rating() {
+  return (
+    <>
+    <div class="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        // activeStyle={{
+        //   background: "darkblue",
+        // }}
+        activeStyle={{
+          textDecoration: "underline"
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          textDecoration: "underline",
+        }}
+      >
+        About
+      </NavLink>
+      
+    </div>
+
+
+    <div id="bdiv1">
+    <img src={'https://c4.wallpaperflare.com/wallpaper/894/492/7/mcdonalds-burger-hd-wallpaper-preview.jpg'}
+     id="main-burger" alt="Delicious-Buger"></img>
+    <footer id="footer">
+      I am a footer
+    </footer>
+    </div>
+  </>
+  );
+}
+
+export default Rating;
