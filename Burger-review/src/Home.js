87558d4ef1008e3971ef7871f349e79e4fd6733c
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-07 14:40:14 -0800

    added placeholder message for home page

--- Burger-review/src/Home.js
+++ Burger-review/src/Home.js
@@ -53,6 +53,8 @@ function Home() {
       
     </div>
 
+    This is where there will be burgers and a welcome message
+
     <div id="bdiv1">
     <img src={'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80'}
      id="main-burger" alt="Delicious-Buger"></img>
