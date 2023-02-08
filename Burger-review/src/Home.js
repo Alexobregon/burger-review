57760979d012c5b4f4c998e8c73e614ce016a246
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-08 12:12:14 -0800

    fixed div issue in About

--- Burger-review/src/Home.js
+++ Burger-review/src/Home.js
@@ -58,7 +58,6 @@ function Home() {
    
     <img src={'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80'}
      id="main-burger" alt="Delicious-Buger"></img>
-      <p>Hello</p>
     <footer id="footer">
       I am a footer
     </footer>
