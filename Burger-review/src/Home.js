54181f86b9c04ffb6122e5797a2f6b5ca926617c
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-10 14:48:58 -0800

    added welcome text to home page

--- Burger-review/src/Home.js
+++ Burger-review/src/Home.js
@@ -52,10 +52,17 @@ function Home() {
       </NavLink>
       
     </div>
+    <div id="welcomeMessage">
+    
+      <h2>
+        We Love Burgers and we know you will too!
+      </h2>
+      
 
-    This is where there will be burgers and a welcome message
-
-   <div>
+    </div>
+  
+   <div id="bdiv">
+   
     <img src={'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80'}
      id="main-burger" alt="Delicious-Buger"></img>
     </div>
